14a26aae73ad0a6471a04fa29d3ed4ba30feb6ab
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-10-31 00:56:20 +0600

    project now completed

--- binary.cpp
+++ binary.cpp
@@ -1,48 +1,47 @@
 #include <stdio.h> 
 
-int binarySearch ( int a [ ] , int x , int low , int high ) {
-
-    if ( low > high ) {
-        return false ;
-    } else {
-        int mid = ( low + high ) / 2 ;
+int binarySearch(int a[],int x,int low,int high){
+    if(low > high){
+        return false;
+    } else{
+        int mid =(low + high)/ 2;
         
-        if ( a [ mid ] == x ) {
+        if(a[mid] == x){
            
-            return mid ;
-        } else if ( x > a [ mid ] ) {
-            return binarySearch ( a , x , mid + 1 , high ) ;
-        } else {
-            return binarySearch ( a , x , low  , mid - 1 ) ;
+            return mid;
+        } else if(x > a[mid]){
+            return binarySearch(a,x,mid + 1,high);
+        } else{
+            return binarySearch(a,x,low,mid - 1);
         }
     }
 }
 
-int main ( ) {
-    int k , n ;
-    scanf ( "%d" , &n ) ;
-    int a [ n + 1 ] ;
 
-    This is single line comment
 
-    for ( int i = 0 ; i < n ; i++ ) {
-        scanf ( "%d" , &a [ i  ] ) ;
+int main (){
+    int k,n;
+    scanf("%d",&n);
+    int a[n + 1];
+
+    //This is single line comment
+    for(int i = 0; i < n; i++){
+        scanf("%d",&a[i]);
     }
     
-    This is single line comment
-
-    for ( int i = 0 ; i < n ; i++ ) {
-        printf ( "%d" , a [ i  ] ) ;
+    /*This is multiple line comment*/
+    for(int i = 0; i < n; i++){
+        printf("%d",a[i]);
     }
 
 
-    printf ( "Give the digit you wanna find" ) ;
+    printf("Give the digit you wanna find");
 
-    scanf ( "%d" , &k ) ;
+    scanf("%d",&k);
     
-    int result = binarySearch ( a , k , 0 , n - 1 ) ,
+    int result = binarySearch(a,k,0,n - 1);
     
-    printf ( "Index is%d value is %d" , result , a [ result ] ) ;
+    printf("Index is %d value is %d",result,a[result]);
 
-    return 0 ;
+    return 0;
 }
